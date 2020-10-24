04c6096377dc8158272c0e78fe686996cb2fb809
jai1408 <jaishankarmishra17@gmail.com>
2020-10-25 01:30:10 +0530

    py101: added Strings

--- hell.py
+++ hell.py
@@ -33,30 +33,48 @@ square()
 turtle.done()'''
 
 # Calender module
-print(calendar.weekheader(3))
+'''print(calendar.weekheader(3))
 print()
-print(calendar.month(2020, 10))
+print(calendar.month(2020, 10))'''
 
 # variables in python
-age = 10
+'''age = 10
 price = 19.95
 first_name = 'Jai'
-is_online = True
+is_online = True'''
 
 # input from CL
-name = input("What is you name? ")
+'''name = input("What is you name? ")
 print("Hello " + name)
-print()
+print()'''
 
 # Type conversion
-birth_year = input("What is you birth year: ")
+'''birth_year = input("What is you birth year: ")
 age = 2020 - int(birth_year)
 print(age)
 print()
-'''int() float() bool() str()'''
+int() float() bool() str()
 first = input("first num: ")
 second = input("second num: ")
 add = float(first) + float(second)
-print("add: " + str(add))
+print("add: " + str(add))'''
 
+# Strings
+print()
+course = "Python for Beginners"
+print(course[0])  # returns the first character
+print(course[1])  # returns the second character
+print(course[-1])  # returns the first character from the end
+print(course[-2])  # returns the second character from the end
 
+print(course.upper())
+print(course)  # strings are immutable in python
+print('Python' in course)
+print(course.replace('for', '4'))
+print(course.find('o'))
+
+print(course[1:5])  # We can slice a string using a similar notation:
+
+name = 'Jai'
+message = f'Hi, my name is {name}'
+print(message)
