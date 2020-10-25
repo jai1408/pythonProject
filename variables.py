1be678cb3a13aee2ade7d8f9ededf31eeae803fe
jai1408 <jaishankarmishra17@gmail.com>
2020-10-25 13:19:23 +0530

    py101: variables assignments

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Dynamic Typing
+'''Python uses dynamic typing, meaning you can reassign variables to different data types.
+This makes Python very flexible in assigning data types;
+it differs from other languages that are statically typed.'''
+my_dogs = 2
+print(my_dogs)
+print(type(my_dogs))
+my_dogs = ['Sammy', 'Oscar']
+print(my_dogs)
+print(type(my_dogs))
+
+# assigning variables
+# reassigning variables
+# find type of variable
